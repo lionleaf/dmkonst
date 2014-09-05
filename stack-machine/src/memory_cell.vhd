0aab6964e666a5e2612e231b0e5faf7614e70ebc
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2014-09-05 17:34:03 +0200

    Clean up (cosmetically) memory_cell and stack

--- stack-machine/src/memory_cell.vhd
+++ stack-machine/src/memory_cell.vhd
@@ -1,48 +1,42 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.defs.all;
 use IEEE.NUMERIC_STD.ALL;
-
+use work.defs.all;
 
 entity memory_cell is
-    port ( 
-           clock : in std_logic;
-           reset  : in std_logic;
-           push   : in std_logic;
-           pop    : in std_logic;
-           data_in  : in  operand_t;
-           data_out : out operand_t;
-           below_data : in operand_t
-           );
+
+    port ( clock      : in  std_logic
+         ; reset      : in  std_logic
+         ; push       : in  std_logic
+         ; pop        : in  std_logic
+         ; data_in    : in  operand_t
+         ; data_out   : out operand_t
+         ; below_data : in  operand_t
+         );
+
 end memory_cell;
 
 architecture behavioral of memory_cell is
-  signal data : operand_t;
-begin
-  
---  buffer_process : process (clock)
---  begin
---    if rising_edge(clock) then
-      data_out <= data;
---    end if;
---  end process;
-  
-  write_process : process (reset, clock, push, pop)
-  begin
-    if reset = '1' then
-      data <= (others => '0');
-    else
-      if rising_edge(clock) then 
-        if pop = '1' then
-          data <= below_data;
-        elsif push = '1' then
-          data <= data_in;
-        end if;
-      end if;
-    end if;
-  end process write_process;
-  
-  
-  
-end Behavioral;
 
+    signal data : operand_t;
+
+begin
+
+    data_out <= data;
+  
+    write_process : process (reset, clock, push, pop)
+    begin
+        if reset = '1' then
+            data <= (others => '0');
+        else
+            if rising_edge(clock) then 
+                if pop = '1' then
+                    data <= below_data;
+                elsif push = '1' then
+                    data <= data_in;
+                end if;
+            end if;
+        end if;
+    end process write_process;
+
+end Behavioral;
--- stack-machine/src/stack.vhd
+++ stack-machine/src/stack.vhd
@@ -5,45 +5,49 @@ use work.defs.all;
 use work.memory_cell;
 
 
-entity stack is
-  
-  generic (
-    size : natural);            -- Maximum number of operands on stack
+entity stack is 
 
-  port (
-    clk       : in  std_logic;
-    rst       : in  std_logic;
-    value_in  : in  operand_t;
-    push      : in  std_logic;
-    pop       : in  std_logic;
-    top       : out operand_t);
+    generic
+        (size       : natural -- Maximum number of operands on stack
+        );
+
+    port
+        ( clk       : in  std_logic
+        ; rst       : in  std_logic
+        ; value_in  : in  operand_t
+        ; push      : in  std_logic
+        ; pop       : in  std_logic
+        ; top       : out operand_t
+        );
 
 end entity stack;
 
 architecture behavioural of stack is
-  type operand_vec is array(0 to size+1) of operand_t;
-  signal wire : operand_vec;
-  signal top_out : operand_t;
+
+    type operand_vec is array(0 to size+1) of operand_t;
+    signal wire : operand_vec;
+    signal top_out : operand_t;
 
 begin
 
-  wire(0) <= (others => '0');
-  wire(size+1) <= value_in;
-  top <= wire(size);
+    wire(0) <= (others => '0');
+    wire(size+1) <= value_in;
+    top <= wire(size);
 
-  memory_cell : for i in 1 to size generate
-    cell : entity work.memory_cell
-      port map (
-        data_in => wire(i+1),
-        data_out => wire(i),
-        below_data => wire(i-1),
-        
-        clock => clk,
-        reset => rst,
-        push => push,
-        pop => pop
-        );
+    memory_cell:
+    for i in 1 to size generate
+        cell:
+        entity work.memory_cell
+            port map
+                (data_in => wire(i+1)
+                ,data_out => wire(i)
+                ,below_data => wire(i-1)
+                
+                ,clock => clk
+                ,reset => rst
+                ,push => push
+                ,pop => pop
+                );
   end generate;
-
 
 end architecture behavioural;
