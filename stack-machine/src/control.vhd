d972d737d57f8a978f7a58d2a457c7de13fd1567
Geir Kulia <geir@kulia.no>
2014-09-05 04:32:27 +0200

    First implementation of control.
    Before testing with stack

--- stack-machine/src/control.vhd
+++ stack-machine/src/control.vhd
@@ -30,10 +30,83 @@ end entity control;
 
 architecture behavioural of control is
 
-  -- Fill in type and signal declarations here.
+  type state_type is (
+    idle,
+    push_result,
+    compute,
+    pop_a, 
+    pop_b,
+    decode,
+    push_operand,
+    fetch);
+  
+  signal current_state : state_type;
+  signal next_state : state_type;
+
 
 begin  -- architecture behavioural
 
-  -- Fill in processes here.
+  process(clk, rst)
+  begin
+		if (rst = '1') then -- Asyncon reset 
+      current_state <= idle;
+		elsif( rising_edge(clk) ) then
+			current_state <= next_state;
+		end if;
+	end process;
+  
+  process(
+    clk,
+    rst,
+    instruction,
+    empty,
+    current_state)
+  begin
+    case current_state is
+      when idle => 
+        read_instruction <= '0';
+        operand_a_wen <= '0';
+        operand_b_wen <= '0';
+        read_instruction <= '0';
+        pop <= '0';
+        push <= '0';
+        -- alu_operation and stack_input_select is don't care
+			
+        if(empty = '1') then
+          next_state <= idle;
+        elsif (empty = '0') then
+          next_state <= fetch;            
+        end if;
+      when fetch =>
+        read_instruction <= '1';
+        next_state <= decode; 
+      when decode =>
+        read_instruction <= '0';
+    
+        if ( instruction(15 downto 8) = (15 downto 8 => '0' )) then -- push 
+					next_state <= push_operand;
+				else -- Not push 
+					next_state <= pop_b;
+				end if;
+      when push_operand =>
+        push <= '1';
+        stack_input_select <= STACK_INPUT_OPERAND;
+        next_state <= idle;
+        operand(7 downto 0) <= instruction(7 downto 0);
+        next_state <= idle;
+      when pop_b =>
+        pop <= '1';
+        operand_b_wen <= '1';
+        next_state <= pop_a;
+      when pop_a =>
+        pop <= '1';
+        operand_a_wen <= '1';
+        next_state <= compute;
+        
+      when others =>
+        next_state <= idle;
+    end case;    
+  end process;
+    
 
 end architecture behavioural;
