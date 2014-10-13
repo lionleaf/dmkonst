01763df3353a8781429b8f9e8754a466b90c4d56
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2014-10-13 21:42:29 +0200

    Change stl to do stl

--- multi-cycle-mips/src/tb_alu.vhd
+++ multi-cycle-mips/src/tb_alu.vhd
@@ -7,62 +7,78 @@ entity tb_alu is
 end tb_alu;
  
 architecture behavior of tb_alu is
+    constant data_width : integer := 32;
+    subtype data_t is signed(data_width-1 downto 0);
+    
+    function num(n: integer) return data_t is begin return to_signed(n, data_width); end num;
+    function str(n: data_t) return string is begin return integer'image(to_integer(n)); end str;
+    function str(n: integer) return string is begin return integer'image(n); end str;
+
+    
     -- component declaration for the unit under test (uut)
     component alu
+    generic
+        (data_width      : integer
+        );
     port
-        ( operand_left   : in      signed(32 downto 0)
-        ; operand_right  : in      signed(32 downto 0)
+        ( operand_left   : in      data_t
+        ; operand_right  : in      data_t
         ; operator       : in      op_t
         ; result_is_zero : out     boolean
-        ; result         : buffer  signed(32 downto 0)
+        ; result         : buffer  data_t
         );
     end component;
     
 
     --inputs
-    signal operand_left  : signed(32 downto 0) := to_signed(42);
-    signal operand_right : signed(32 downto 0) := (others => '0');
+    signal operand_left  : data_t;
+    signal operand_right : data_t;
     signal operator      : op_t;
 
  	--outputs
     signal result_is_zero : boolean;
-    signal result         : signed(32 downto 0);
+    signal result         : data_t;
  
     --clocks
     constant clock_period : time := 10 ns;
-    signal   clock        : boolean;
- 
+
+    signal expected_result : integer;
 begin
  
     -- instantiate the unit under test (uut)
-    uut: alu port map
-        ( operand_left   => operand_left
-        , operand_right  => operand_right
-        , operator       => operator
-        , result_is_zero => result_is_zero
-        , result         => result
-        );
-
-    -- clock process definitions
-    clock_process :process
-    begin
-		clock <= true;
-		wait for clock_period/2;
-		clock <= false;
-		wait for clock_period/2;
-    end process;
- 
+    uut: alu
+        generic map
+            (data_width      => data_width
+            )
+        port map
+            ( operand_left   => operand_left
+            , operand_right  => operand_right
+            , operator       => operator
+            , result_is_zero => result_is_zero
+            , result         => result
+            );
 
     -- stimulus process
     stim_proc: process
     begin		
-        -- hold reset state for 100 ns.
-        wait for 100 ns;	
-
-        wait for clock_period*10;
-
-        -- insert stimulus here 
-
+        report "====== Test starting ======";
+        
+        operator <= op_add;
+        operand_left <= num(1);
+        operand_right <= num(2);
+        expected_result <= 3;
+        wait for clock_period;
+        assert result = expected_result report str(operand_left) &"+"& str(operand_right) &" should be "& str(expected_result);
+        assert result_is_zero = false report "0 + 0 should give result_is_zero";
+      
+        --for i in integer'low to integer'high loop
+        --for j in integer'low to integer'high loop
+            --report integer'image(i);
+        --end loop;
+        --end loop;
+        
+        -- end of simulation
+        report "====== Test completed successfully ======";
         wait;
     end process;
 
