3d1fe2cd30112d63a0708b8cd63a9406b62b244e
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-10-13 15:37:13 +0200

    Make ALU compatible with everything else
    Fix incompatible types and decode alu opcode

--- multi-cycle-mips/src/defs.vhdl
+++ multi-cycle-mips/src/defs.vhdl
@@ -11,4 +11,21 @@ package defs is
         , op_slt
         );
 
+    function to_op_t (opcode : std_logic_vector(5 downto 0))
+                      return op_t;    
 end package defs;
+
+package body defs
+    is function to_op_t (opcode : std_logic_vector(5 downto 0))
+                         return op_t is 
+    begin 
+        case opcode is 
+           when "100000" => return op_add;
+           when "100010" => return op_sub;
+           when "101010" => return op_slt;
+           when "100100" => return op_and; 
+           when "100101" => return op_or;
+           when others   => return op_sub;
+        end case;
+    end to_op_t;
+ end defs;
