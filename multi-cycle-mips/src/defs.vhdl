c715ddd56823578f04bfcd02452763f25ba8446d
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2014-10-13 13:56:28 +0200

    Add entity ALU and op_t to defs.vhdl

--- multi-cycle-mips/src/defs.vhdl
+++ multi-cycle-mips/src/defs.vhdl
@@ -3,5 +3,12 @@ use ieee.std_logic_1164.all;
 
 package defs is
 
+    type op_t is
+        ( op_add
+        , op_sub
+        , op_and
+        , op_or
+        , op_slt
+        );
 
 end package defs;
