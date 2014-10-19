7ef2c3ae47bdec56b0e6f927a05c041c83a38251
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-10-19 21:27:15 +0200

    Moved alu control logic to separate module

--- multi-cycle-mips/src/ALU.vhd
+++ multi-cycle-mips/src/ALU.vhd
@@ -12,7 +12,7 @@ entity alu is
     port
         ( operand_left   : in      signed (data_width - 1 downto 0)
         ; operand_right  : in      signed (data_width - 1 downto 0)
-        ; operator       : in      op_t
+        ; operator       : in      alu_funct_t
         ; result_is_zero : out     boolean
         ; result         : buffer  signed (data_width - 1 downto 0) := to_signed(0, data_width)
         );
@@ -27,12 +27,12 @@ begin
     process (operator, operand_left, operand_right)
     begin
         case operator is
-            when op_add => result <= operand_left +   operand_right;
-            when op_sub => result <= operand_left -   operand_right;
-            when op_and => result <= operand_left and operand_right;
-            when op_or  => result <= operand_left or  operand_right;
-            when op_sll16 => result <= operand_right sll 16;
-            when op_slt =>
+            when alu_add => result <= operand_left +   operand_right;
+            when alu_sub => result <= operand_left -   operand_right;
+            when alu_and => result <= operand_left and operand_right;
+            when alu_or  => result <= operand_left or  operand_right;
+            when alu_sll16 => result <= operand_right sll 16;
+            when alu_slt =>
                 if operand_left < operand_right
                     then result <= to_signed(1, data_width);
                     else result <= to_signed(0, data_width);
