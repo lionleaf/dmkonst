fdcc39a52b95e12c02ec446266131ef9fb0b5c66
Geir Kulia <geir@kulia.no>
2014-11-08 17:22:57 +0100

    made body of execute module

--- pipeline-mips/src/execute.vhd
+++ pipeline-mips/src/execute.vhd
@@ -5,25 +5,49 @@ use work.defs.all;
 
 entity execute is
 	port
-		( clk					: std_logic
-		; reset				: std_logic
-		; incremented_pc	: addr_t
-		; data_1				: word_t
-		; data_2				: word_t
-		; instructions  : word_t
-		; alu_source		: std_logic
-		; alu_operation	: alu_funct_t
-		; alu_result		: word_t
-		; alu_zero			: std_logic
-		; branch_address	: addr_t
+		( clk					: in	std_logic
+		; reset				: in	std_logic
+		; incremented_pc	: in	addr_t
+		; data_1				: in	word_t
+		; data_2				: in	word_t
+		; instructions  	: in 	word_t
+		; alu_source		: in 	std_logic
+		; alu_operation	: in 	alu_funct_t
+		; alu_result		: out	word_t
+		; alu_zero			: out	std_logic
+		; branch_address	: out	addr_t
 		)
 	;
 end execute;
 
 architecture Behavioral of execute is
 
+	alias immediate		: std_logic_vector(15 downto 0) is instructions(15 downto 0);
+	signal right_oprand	: word_t;
+
 begin
-
-
+	
+	right_oprand <= immediate when alu_source = '1'
+				else	 data_2;
+	
+	branch_address_select:
+		entity work.branch_address_select
+		port map
+			( left_oprand   => incremented_pc 
+			, right_oprand  => immediate 
+			, result        => branch_address
+			)
+		;
+		
+	alu:
+		entity work.alu
+		port map
+			( operand_left   => data_1
+			, operand_right  => right_oprand
+			, operator       => alu_operation
+			, result_is_zero => alu_zero
+			, result         => alu_result
+			)
+		;
+	
 end Behavioral;
-
