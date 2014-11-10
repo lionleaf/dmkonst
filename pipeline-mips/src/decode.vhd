3aeafacb0b11ae4d72b58e4094fdfbe1984067f9
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-11-10 16:19:00 +0100

    Fix funct in decode on R type

--- pipeline-mips/src/decode.vhd
+++ pipeline-mips/src/decode.vhd
@@ -12,6 +12,7 @@ entity decode is
         ; mem_wen     : out  std_logic
         ; reg_wen     : out  std_logic
         ; inst_type_I : out  std_logic
+        ; imm_to_alu  : out  std_logic
         ; alu_funct   : out  alu_funct_t
         ; alu_shamt   : out  alu_shamt_t
         );
@@ -22,6 +23,8 @@ end decode;
 
 architecture Behavioral of decode is
   alias opcode is instruction(31 downto 26);
+  alias instruction_funct is instruction(5 downto 0);
+  alias instruction_shamt is instruction(10 downto 6);
 begin
 
   process (instruction, processor_enable)
@@ -32,6 +35,7 @@ begin
      mem_wen     <= '0';
      reg_wen     <= '0';
      inst_type_I <= '0';
+     imm_to_alu <= '0';
      alu_funct   <= (others => '0');
      alu_shamt   <= (others => '0');
      
@@ -40,30 +44,33 @@ begin
        case opcode is
           when op_add => -- ALU operation (and, or, add, sub, slt, sll)
               reg_wen <= '1';
+              alu_funct <= instruction_funct;
 
           when op_beq => -- beq branch if equal
               branch_en <= '1';
-              inst_type_I <= '1';
               alu_funct <= alu_sub;
 
 --            when "000010" => -- jump
 --                reg_dest <= '1';
 --                pc_control <= jump;
           when op_lw => -- lw load word
-              mem_to_reg <= '1';
-              alu_funct <= alu_add;
+              mem_to_reg  <= '1';
+              alu_funct   <= alu_add;
               inst_type_I <= '1';
+              imm_to_alu  <= '1';
               reg_wen     <= '1';
 
           when op_sw => -- sw store word
-              alu_funct <= alu_add;
+              alu_funct   <= alu_add;
               inst_type_I <= '1';
-              mem_wen <= '1';
+              imm_to_alu  <= '1';
+              mem_wen     <= '1';
 
           when op_lui => -- lui load upper imm
-              alu_funct <= alu_sll;
-              alu_shamt <= std_logic_vector(to_unsigned(16, 6));
+              alu_funct   <= alu_sll;
+              alu_shamt   <= std_logic_vector(to_unsigned(16, 6));
               inst_type_I <= '1';
+              imm_to_alu  <= '1';
               reg_wen     <= '1';
 
           when others => --Error, should not happen
